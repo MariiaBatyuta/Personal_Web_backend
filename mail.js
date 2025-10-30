aea0ddeb4c2ac21e09785161262bc7af574f6623
MariiaBatyuta <mashabatyuta.work@gmail.com>
2025-10-30 12:05:13 +0100

    add n8n part

--- mail.js
+++ mail.js
@@ -12,6 +12,15 @@ export const sendMailToContact = async (email, subject, text) => {
       reply_to: email, 
       text: `From: ${email}\n\n${text}`,
     });
+    await fetch("https://n8n-automation-iuts.onrender.com/webhook-test/contact-message", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        from: email,
+        subject,
+        text
+      })
+    });
 
     console.log("✅ Email sent successfully via Resend");
   } catch (error) {
